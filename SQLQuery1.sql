fc44e1a4d2ed0a69a722b17c99962019e141c015
jaded-git <jadehoey9@gmail.com>
2025-12-16 12:06:19 -0600

    implemented services webpage with validation and tested adding/deleting/editing services, ManagerStyling.css handled all styling!

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿DELETE FROM [User];
+DELETE FROM [Appointment];
+DELETE FROM [Service];
+DELETE FROM [ServicePerformingEmployeeService];
+DBCC CHECKIDENT('User', RESEED, 0);
+DBCC CHECKIDENT('Appointment', RESEED, 0);
+DBCC CHECKIDENT('Service', RESEED, 0);
+DBCC CHECKIDENT('ServicePerformingEmployeeService', RESEED, 0);
